6bbd2d85a9a862eef799a4f0832210c01342b23e
Mohammed Boutarbouch <mohamedboutarbouch48@gmail.com>
2026-04-17 20:31:46 +0100

    feat: reading the meals from the async storage

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
-import { Text, ScrollView } from 'react-native';
+import { getMeals, Meal } from '@/storage/meals';
 import { globalStyles } from '@/styles/global';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { ScrollView, Text } from 'react-native';
 import HomeHeader from '../components/home-header';
 import MacroGrid from '../components/macro-grid';
 import RecentMeals from '../components/recent-meals';
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-      <MacroGrid />
-      <RecentMeals />
+      <MacroGrid meals={meals} />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
