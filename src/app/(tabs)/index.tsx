ea106141368d3c3e87d726eaccdee2c29c2297de
Mohammed Boutarbouch <mohamedboutarbouch48@gmail.com>
2026-04-17 19:34:32 +0100

    feat: recent meals added to the home screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { Text, ScrollView } from 'react-native';
 import { globalStyles } from '@/styles/global';
 import { HomeHeader } from '../components/home-header';
 import { MacroGrid } from '../components/macro-grid';
+import { RecentMeals } from '../components/recent-meals';
 
 export default function HomeScreen() {
   return (
@@ -9,6 +10,7 @@ export default function HomeScreen() {
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
       <MacroGrid />
+      <RecentMeals />
     </ScrollView>
   );
 }
