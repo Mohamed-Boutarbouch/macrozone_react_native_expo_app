ce037b51ca30163de1e6dc4ec4b7fac7ab8ed217
Mohammed Boutarbouch <mohamedboutarbouch48@gmail.com>
2026-04-17 19:00:19 +0100

    feat: meals page added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
 import { globalStyles } from '@/styles/global';
-import { Text, View } from 'react-native';
-import { HomeHeader } from './components/HomeHeader';
+import { Link } from 'expo-router';
+import { Text, ScrollView } from 'react-native';
+import { HomeHeader } from './components/home-header';
 
 export default function HomeScreen() {
   return (
-    <View style={globalStyles.container}>
+    <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-    </View>
+      <Link href='/meals' style={{ fontSize: 18, color: '#007bff' }}>
+        Go to Meals
+      </Link>
+    </ScrollView>
   );
 }
