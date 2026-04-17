1534143dfe5582e5a277d1b2531c8c4554d4312d
Mohammed Boutarbouch <mohamedboutarbouch48@gmail.com>
2026-04-17 19:27:19 +0100

    feat: macro grid component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,14 @@
 import { Text, ScrollView } from 'react-native';
-import { Link } from 'expo-router';
 import { globalStyles } from '@/styles/global';
 import { HomeHeader } from '../components/home-header';
+import { MacroGrid } from '../components/macro-grid';
 
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-      <Link href='/add-meal' style={{ fontSize: 18, color: '#007bff' }}>
-        Go to Add Meal
-      </Link>
+      <MacroGrid />
     </ScrollView>
   );
 }
